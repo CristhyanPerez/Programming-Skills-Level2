38abebc71c5289c00bfd11c9ebe8fd494cea4387
Cristhyan Perez <pevicristhyan@gmail.com>
2024-02-07 06:33:40 -0500

    Update Ex01 - Big Six - Function to assign values to the two dataframes - The main structure is missing

--- Big_six.py
+++ Big_six.py
@@ -23,7 +23,42 @@ def create_df():
     df_02 = pd.DataFrame(columns=header_games) #Create the second dataFrame
     return df_01, df_02
 
-df_points, df_games = create_df()
+#Function to assign values specific in the main dataframe
+def assign_first_dataframe(first_team, second_team, goals_01, goals_02, first_df):
+    index_01 = first_df.index[first_df["Team"] == first_team]
+    index_02 = first_df.index[first_df["Team"] == second_team]
+    if goals_01 > goals_02:
+        first_df.loc[index_01, "Points"] += 3
+        first_df.loc[index_01, "Won"] += 1
+        first_df.loc[index_02, "Lost"] += 1
+    if goals_02 > goals_01:
+        first_df.loc[index_02, "Points"] += 3
+        first_df.loc[index_02, "Won"] += 1
+        first_df.loc[index_01, "Lost"] += 1
+    else:
+        first_df.loc[index_01, "Points"] += 1
+        first_df.loc[index_02, "Points"] += 1
+        first_df.loc[index_01, "Drawn"] += 1
+        first_df.loc[index_02, "Drawn"] += 1
+    first_df.loc[index_01, "GF"] += goals_01
+    first_df.loc[index_01, "GA"] += goals_02
+    first_df.loc[index_02, "GF"] += goals_02
+    first_df.loc[index_02, "GA"] += goals_01
+    first_df.loc[index_01, "Played"] += 1
+    first_df.loc[index_02, "Played"] += 1
+
+
+#Function to evaluate the 3 games between two teams
+def games_played(number_games, first_team, second_team, first_df, second_df):
+    for i in range(number_games):
+        goals_team_01 = random.randint(0,5)
+        goals_team_02 = random.randint(0,5)
+        list_row = [first_team, goals_team_01, second_team, goals_team_02]
+        #Assign a row to the games dataframe
+        second_df.loc[len(second_df)]= list_row
+        #Function to assign values to the first dataframe
+        assign_first_dataframe(first_team, second_team, goals_team_01, goals_team_02, first_df)
+
 
 #Function to traverse the list. This function allows you to face different teams without repeating
 def big_six_random(six_teams, df_01, df_02):
@@ -33,9 +68,10 @@ def big_six_random(six_teams, df_01, df_02):
             if j > i :
                 team_01 = six_teams[i]
                 team_02 = six_teams[j]
-                #funcion que se tiene que repetir tres veces
-                goals_team_01 = random.randint(0,5)
-                goals_team_02 = random.randint(0,5)
-                print(team_01, team_02, goals_team_01, goals_team_02)
+                games_played(3, team_01, team_02, df_01, df_02)
+    #Add goal difference
+    for i in range(lenght_teams):
+        df_01.loc[i, "GD"] = df_01.loc[i, "GF"] - df_01.loc[i, "GA"]
+    #Sort by points from highest to lowest
+    df_01.sort_values(by='Points', ascending=False, inplace=True)
 
-big_six_random(bix_six, df_points, df_games)
