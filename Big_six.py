a46151a3dc20c9689d65c12d0b606ef27fdfc21c
Cristhyan Perez <pevicristhyan@gmail.com>
2024-02-07 06:47:50 -0500

    Update Ex01 - Big Six - The basic main structure - A function is missing to access matches between twwo particular teams

--- Big_six.py
+++ Big_six.py
@@ -6,6 +6,8 @@ import random
 bix_six = ["Manchester United", "Liverpool", "Arsenal", "Chelsea", "Manchester City", "Tottenham Hotspur"]  #List of teams
 headers_teams = ["Team", "Played", "Won", "Drawn", "Lost", "GF", "GA", "GD", "Points"]  #Column header of the first dataframe
 header_games = ["Team_01", "Goals_01", "Team_02", "Goals_02"]   #Column header of the second dataframe
+available_yes_no = ["y", "yes", "YES", "n", "no", "NO"]
+options_menu = ["1", "2"]
 
 #Declare the data of the first dataframe
 data_teams = [
@@ -22,6 +24,18 @@ def create_df():
     df_01 = pd.DataFrame(data_teams, columns=headers_teams) #Create the first dataFrame
     df_02 = pd.DataFrame(columns=header_games) #Create the second dataFrame
     return df_01, df_02
+
+#Given a question and a list of answers, the function will evaluate if the
+#answer to the question is within the list
+def check_answer(question, list_answers):
+    start_check = False
+    while start_check == False:
+        option_user = input(question)
+        if option_user in list_answers:
+            start_check = True
+        else:
+            print("Wrong option. Let's go again\n")
+    return option_user
 
 #Function to assign values specific in the main dataframe
 def assign_first_dataframe(first_team, second_team, goals_01, goals_02, first_df):
@@ -75,3 +89,41 @@ def big_six_random(six_teams, df_01, df_02):
     #Sort by points from highest to lowest
     df_01.sort_values(by='Points', ascending=False, inplace=True)
 
+#Message to exit the program
+def message_custom(sentence):
+    print()
+    print(sentence)
+    print("Thank you")
+    print("Come back soon\n")
+
+#Initial menu
+menu_start = """
+
+"""
+
+#Main Function
+def main():
+    reset_main_menu = True
+    while reset_main_menu == True:
+
+        print(menu_start)
+        chosen_option = check_answer("Chose one of the options (1, 2): ", options_menu)
+        if chosen_option == "1":
+            print("opcion_1")
+        elif chosen_option == "2":
+            print("opcion_2")
+        question_menu = check_answer("\nDo you want to return to the main menu? (y/n): ", available_yes_no)
+        if question_menu in available_yes_no[0:3]:
+            print("\nLet's start again..!!!!\n")
+            reset_main_menu = True
+        else:        
+            message_custom("")
+            reset_main_menu = False
+
+#Entry point
+if __name__ == "__main__":
+    main()
+
+#Hace falta una funcion para acceder a los partidos entre dos equipos en particular
+    
+#Hay que probarlo con un dataframe más pequeño de 3 equipos para poder ver el funcionamiento
